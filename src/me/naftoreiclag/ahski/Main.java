b6c24975e25f85fa840f44a42c6225c6f00da5fb
Naftoreiclag <naftoreiclag@gmail.com>
2012-08-14 13:26:24 -0700

    Added placeholder Main.java
    I already have the code, I just need to upload it.
    The game is actually harder than I intended it to be. Nobody I asked has
    beat it yet!

---
+++ src/me/naftoreiclag/ahski/Main.java
@@ -0,0 +1,9 @@
+package me.naftoreiclag.ahski;
+
+public class Main
+{
+	public static void main(String[] Args)
+	{
+		System.out.println("I just have this here as a placeholder. I actually already have the code, I just need to find some time to add it.");
+	}
+}
